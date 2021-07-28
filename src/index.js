305f3ff1e729a09a78ee36f89986214987f0e2ab
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-28 13:20:51 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { theme } from "./theme";
+import { GlobalStyle} from "./GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
