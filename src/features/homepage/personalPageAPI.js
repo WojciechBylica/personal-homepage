efa1c65ad9e01f7a1f088ff9ea36ffacac18815a
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-01 23:59:01 +0200

    deleted

--- src/features/homepage/personalPageAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const gitHubAPIURL = "https://api.github.com";
-
-export const getRepositories = username =>
-    axios.get(`${gitHubAPIURL}/users/${username}/repos`)
-        .then(response => response.data);
