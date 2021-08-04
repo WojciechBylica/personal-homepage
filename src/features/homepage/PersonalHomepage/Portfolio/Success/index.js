e513c22064229ef5fdd35338894a921632ab549d
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-04 17:55:51 +0200

    demo link corr

--- src/features/homepage/PersonalHomepage/Portfolio/Success/index.js
+++ src/features/homepage/PersonalHomepage/Portfolio/Success/index.js
@@ -4,14 +4,15 @@ import { Article, Description, Header, Link, Links, Section } from "./styled";
 
 const Success = () => {
     const repositories = useSelector(selectRepositories);
-
+    const demo = `https://wojciechbylica.github.io/`;
+    
     return (
         <Section>
             {Object.keys(repositories).map(repo =>
                 <Article key={repo}>
                     <Header>{repositories[repo].name}</Header>
                     <Description>{repositories[repo].description}</Description>
-                    <Links>Demo: <Link href={repositories[repo].url} target="_blank" rel="noreferrer noopener">{repositories[repo].url}</Link></Links>
+                    <Links>Demo: <Link href={`${demo}${repositories[repo].name}`} target="_blank" rel="noreferrer noopener">{`${demo}${repositories[repo].name}`}</Link></Links>
                     <Links>Repo: <Link href={repositories[repo].clone_url} target="_blank" rel="noreferrer noopener">{repositories[repo].clone_url}</Link></Links>
                 </Article>
             )}
