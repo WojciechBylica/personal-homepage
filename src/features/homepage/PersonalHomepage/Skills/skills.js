d59da1ae70fd0d2e5c3df28fbac131bd453f0e9b
Wojciech Bylica <wojtekbyl@gmail.com>
2022-05-02 22:22:30 +0200

    skills upd

--- src/features/homepage/PersonalHomepage/Skills/skills.js
+++ src/features/homepage/PersonalHomepage/Skills/skills.js
@@ -1,4 +1,10 @@
 export const skillsList = [
+  'React',
+  'React with hooks',
+  'Redux (Toolkit)',
+  'Redux-Saga',
+  'JavaScript: ES6+',
+  'Next',
   'Semantic & accessible HTML',
   'Responsive Web Design',
   'Teamwork',
@@ -8,26 +14,21 @@ export const skillsList = [
   'CSS Grid',
   'CSS flexbox',
   'React Router',
-  'Redux-Saga',
-  'Redux (Toolkit)',
-  'React Hooks',
   'Error handling',
   'Working with API (fetch, axios)',
-  'JavaScript: ES6+',
   'Promises, Async/Await',
   'GitHub Pull Requests & Review',
   'Trello',
   'Scrum',
-  'NPM',
-  'React',
+  'npm/yarn',
   'GIT',
+  'E2E testing - Cypress',
+  'Unit testing - React Testing Library, Enzyme',
 ];
 
-export const nextSkillsList = [
-  'React Context',
-  'Node.js',
-  'Cypress',
-  'Unit testing',
-  'TypeScript',
-  'JS classes',
+export const nextSkillsList = ['Node.js', 'TypeScript'];
+
+export const experience = [
+  'National Institute Of Communication: since 05.2022',
+  'Pro Novis:  09.2021-03.2022',
 ];
