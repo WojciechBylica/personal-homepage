8729627c860af8aa7104725a5ba39280ba8a72d4
Wojciech Bylica <wojtekbyl@gmail.com>
2022-05-15 22:07:19 +0200

    defined type of repositories

--- src/features/homepage/PersonalHomepage/Portfolio/Success/index.tsx
+++ src/features/homepage/PersonalHomepage/Portfolio/Success/index.tsx
@@ -3,31 +3,31 @@ import { selectRepositories } from '../../../personalHomepageSlice';
 import { Article, Description, Header, Link, Links, Section } from './styled';
 
 const Success = () => {
-  const repositories:any = useSelector(selectRepositories);
+  const repositories = useSelector(selectRepositories);
   const demo = `https://wojciechbylica.github.io/`;
 
   return (
     <Section>
-      {Object.keys(repositories).map((repo) => (
-        <Article key={repo}>
-          <Header>{repositories[repo].name}</Header>
-          <Description>{repositories[repo].description}</Description>
+      {repositories.map((repo) => (
+        <Article key={repo.id}>
+          <Header>{repo.name}</Header>
+          <Description>{repo.description}</Description>
           <Links>
             Demo:{' '}
             <Link
-              href={`${demo}${repositories[repo].name}`}
+              href={`${demo}${repo.name}`}
               target="_blank"
               rel="noreferrer noopener"
-            >{`${demo}${repositories[repo].name}`}</Link>
+            >{`${demo}${repo.name}`}</Link>
           </Links>
           <Links>
             Repo:{' '}
             <Link
-              href={repositories[repo].clone_url}
+              href={repo.clone_url}
               target="_blank"
               rel="noreferrer noopener"
             >
-              {repositories[repo].clone_url}
+              {repo.clone_url}
             </Link>
           </Links>
         </Article>
